f7dea391d556b3b440277e3b539c8bb73f1c2f94
Yuta-K19418 <yuta.k.19418@gmail.com>
2021-08-07 13:18:12 +0900

    set foreign key

--- models/sentenses.go
+++ models/sentenses.go
@@ -5,10 +5,10 @@ import (
 )
 
 type Sentense struct {
-	SentenseID string `json:"sentense_id" gorm:"primarykey" sql:"type:uuid"`
-	Title      string `json:"title" binding:"required"`
-	Sentense   string `json:"sentense" binding:"required"`
-	UserID     string `json:"sub" binding:"required"`
+	SentenseID string `json:"sentense_id" gorm:"primarykey;" sql:"type:uuid"`
+	Title      string `json:"title" binding:"required" gorm:"not null"`
+	Sentense   string `json:"sentense" binding:"required" gorm:"not null"`
+	UserID     string `json:"sub" binding:"required" gorm:"not null"`
 	Created_at time.Time
 	Updated_at time.Time
 	Deleted_at *time.Time
--- models/users.go
+++ models/users.go
@@ -5,8 +5,8 @@ import (
 )
 
 type User struct {
-	Sub        string `json:"sub" binding:"required" gorm:"primarykey"`
-	Name       string `json:"name" binding:"required"`
+	Sub        string `json:"sub" binding:"required" gorm:"unique;primarykey;not null"`
+	Name       string `json:"name" binding:"required" gorm:"not null"`
 	Created_at time.Time
 	Updated_at time.Time
 	Deleted_at *time.Time
